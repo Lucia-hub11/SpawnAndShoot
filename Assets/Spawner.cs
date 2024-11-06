c158cc512ce901c831024d06c4a32ef74747d6ee
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-11-06 09:04:51 +0100

    size, gradient y rot

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -7,29 +7,44 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     public GameObject Prefab;
-    
 
+    public Gradient Gradient;
     // Update is called once per frame
     void Update()
     {
         Move();
         if (Input.GetKeyDown(KeyCode.Space))
-            SpawnMany(10);
+            SpawnInPlane(10);
+    }
+
+    void SpawnInPlane(int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            float x = Random.Range(-20f, 20f);
+            float y = 20;
+            float z = Random.Range(-20f, 20f);
+            Vector3 pos = new Vector3(x, y, z);
+            Quaternion rot = Random.rotation;
+            float s = Random.Range(0.5f, 5f);
+            Vector3 size = new Vector3(s, s, s);
+            SpawnOne(pos, rot, size);
+        }
     }
     private void SpawnMany(int n)
     {
-        Vector3 pos = transform.position;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if(ShouldSpawn())
-                    SpawnOne(pos);
-                pos.x += 1;
-            }
-            pos.x = transform.position.x;
-            pos.y += 1;
-        }
+        //Vector3 pos = transform.position;
+        //for (int i = 0; i < n; i++)
+        //{
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //        if(ShouldSpawn())
+        //            SpawnOne(pos);
+        //        pos.x += 1;
+        //    }
+        //    pos.x = transform.position.x;
+        //    pos.y += 1;
+        //}
     }
     public float FillValue = 0.9f;
 
@@ -38,9 +53,11 @@ public class Spawner : MonoBehaviour
         return Random.value <  FillValue;
     }
 
-    private void SpawnOne(Vector3 pos)
+    private void SpawnOne(Vector3 pos, Quaternion rot, Vector3 size)
     {
-        Instantiate(Prefab, pos, Quaternion.identity);
+        GameObject go = Instantiate(Prefab, pos, rot);
+        go.GetComponent<Renderer>().material.color = Gradient.Evaluate(Random.value)/*new Color(Random.value, Random.value, Random.value)*/;
+        go.transform.localScale = size;
     }
     private void SpawnOne()
     {
