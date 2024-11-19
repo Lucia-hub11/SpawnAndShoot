b499abb77ca2de5b99b5ad0a5f8960025b349a31
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-11-19 11:33:27 +0100

    max health

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,26 @@ public class PlayerHealth : MonoBehaviour, ITakeDamage
     private float _currentHealth;
     private float _maxHealth =100;
 
+    public static Action OnDeath;
+
+    private bool _dead;
     private void Start()
     {
         _currentHealth = _maxHealth;
     }
     public void TakeDamage(float amount)
     {
+        _currentHealth -= amount;
         Debug.Log("AUA" + _currentHealth);
+        if(_currentHealth <=0 && !_dead)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _dead = true;
+        OnDeath?.Invoke();
     }
 }
